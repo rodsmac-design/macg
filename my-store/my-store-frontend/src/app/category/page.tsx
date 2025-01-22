9f3fc98602269a9cd628cb117bb6434aa0e4ccb2
rodsmac-design <rodmacgregor@outlook.com>
2025-01-23 00:01:45 +1300

    Delete my-store/my-store-frontend directory

--- my-store/my-store-frontend/src/app/category/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function CategoryPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Categories</h1>
-      <p>Filter through our categories to find what you need.</p>
-    </div>
-  );
-}
-
